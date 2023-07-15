ef8023fd689fa40396e19f672f3cc26f61b66d51
조하담 <hadamcho97@gmail.com>
2023-07-15 23:55:16 +0900

    Add getAllBoards handler

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,9 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardService: BoardsService) {}
+
+    @Get()
+    getAllBoard() {
+        return this.boardService.getAllBoards();
+    }
 }
 
 // 아래 코드와 동일하게 동작한다.
